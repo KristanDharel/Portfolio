dc64719db4678c5e8967ed09c95e9bea7467dbff
Kristan <dharelkristan@gmail.com>
2024-02-03 22:11:49 +0545

    my latest changes

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -24,41 +24,8 @@ const Contact = () => {
       </div>
       <div className="contactWithme">
         <section className="faceb">
-          <div className="expertise">
-            <strong className="exhead">WHERE TO FIND ME:</strong>
-            <br></br>
-            <ul className="skill1">
-              <a
-                href="https://www.facebook.com/kristan.dharel?mibextid=2JQ9oc"
-                target="blank"
-              >
-                <img src="facebook .png" className="face" alt="facebook" />
-              </a>
-              <a href="https://github.com/KristanDharel" target="blank">
-                <img
-                  src="github.svg"
-                  className="insta"
-                  alt="insta"
-                  style={{ height: "40px" }}
-                />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/kristan-dharel-298607252/"
-                target="blank"
-              >
-                <img src="link.png" className="link" alt="link" />
-              </a>
-            </ul>
-          </div>
-          LinkedIn
-          <br />
-          Facebook
-          <br />
-          Github
-          <br />
-          Gmail
-          <br />
-          Phone Number
+          You can find me on: <br></br>
+          LinkedIn Facebook Github Gmail Phone Number
         </section>
       </div>
     </div>
